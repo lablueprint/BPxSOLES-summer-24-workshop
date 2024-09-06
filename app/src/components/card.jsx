91334200f8fd0c1708e98277ba8d7cec25abb4b6
Ryan Yeo <ryandc.yeo@gmail.com>
2024-09-05 18:14:15 -0700

    card structure

--- app/src/components/card.jsx
+++ app/src/components/card.jsx
@@ -2,7 +2,40 @@ import React from "react";
 import "./card.css";
 
 const Card = () => {
-  return <div>hello workshop!</div>;
+  return (
+    <div className="card">
+      <div className="card-header">
+        <img src="/card-image.png" />
+      </div>
+      <div className="card-body">
+        <div className="title">Avocado Toast</div>
+        <div className="tags">
+          <div className="tag" style={{ "background-color": "#d3e1c7" }}>
+            🥑 Avocado
+          </div>
+          <div className="tag" style={{ "background-color": "#dad4c4" }}>
+            🧄 Garlic
+          </div>
+          <div className="tag" style={{ "background-color": "#f0dcbf" }}>
+            🥖 Craft Bread
+          </div>
+        </div>
+        <div className="content">
+          A top choice among our customers: delicious, healthy, and an amazing
+          part of any morning routine!
+        </div>
+        <div className="bottom">
+          <div className="price">15</div>
+          <button className="cart-btn">
+            Add to cart
+            <span>
+              <img src="/cart-icon.png" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
